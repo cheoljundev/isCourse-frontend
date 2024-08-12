41f7fe4e9b61e7773435b92c71ed7c54296fed31
cheoljundev <cheoljundev@gmail.com>
2024-08-12 20:56:55 +0900

    fix: CourseSavedList 타이틀 수정

--- src/components/user/CourseSavedList.jsx
+++ src/components/user/CourseSavedList.jsx
@@ -4,7 +4,7 @@ import CourseSavedListBody from "./CourseSavedListBody.jsx";
 export default function CourseSavedList() {
   return (
     <section className="px-4">
-      <h2 className="text-xl font-semibold mb-4">유저 코스</h2>
+      <h2 className="text-xl font-semibold mb-4">가본 코스 확인</h2>
       {/*<CourseListBodySkeleton/>*/}
       <CourseSavedListBody/>
     </section>
