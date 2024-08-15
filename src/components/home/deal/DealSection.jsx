6699ba9ace1ccad52e8ed634489dde037956b173
cheoljundev <cheoljundev@gmail.com>
2024-08-16 07:06:23 +0900

    feat: Main Deal 정보 가져오기

--- src/components/home/deal/DealSection.jsx
+++ src/components/home/deal/DealSection.jsx
@@ -1,14 +1,30 @@
 import DealSectionItemSkeleton from "./DealSkeleton.jsx";
 import DealSectionItem from "./DealSectionItem.jsx";
+import ky from "ky";
+import {useEffect, useState} from "react";
 
 export default function DealSection() {
+  const [deals, setDeals] = useState(null); // deals 데이터 상태
+  const [loading, setLoading] = useState(true); // 로딩 상태
+
+  useEffect(() => {
+    // 데이터 가져오기
+    ky.get("http://localhost:8080/api/deal")
+      .json()
+      .then((data) => {
+        setDeals(data); // 데이터 저장
+        setLoading(false); // 로딩 완료
+      })
+      .catch((error) => {
+        console.error("Failed to fetch deals", error);
+        setLoading(false); // 실패 시에도 로딩 완료
+      });
+  }, []);
   return (
     <section className="mb-8">
       <h2 className="text-xl font-semibold mb-4">오늘의 딜</h2>
-      {/*<DealSectionItemSkeleton/>*/}
-      <DealSectionItem/>
-      <DealSectionItem/>
-      <DealSectionItem/>
+      { loading && <DealSectionItemSkeleton/> }
+      { !loading && deals && deals.map((deal) => <DealSectionItem key={deal.id} deal={deal}/>) }
     </section>
   );
 }
--- src/components/home/deal/DealSectionItem.jsx
+++ src/components/home/deal/DealSectionItem.jsx
@@ -1,13 +1,13 @@
 export default function DealSectionItem({deal}) {
   return <div className="flex gap-x-4 mb-4">
-    <img className="rounded-xl flex-shrink-0" src="https://picsum.photos/100" alt="딜 이미지"/>
+    <img className="rounded-xl flex-shrink-0 w-32 h-32 object-cover" src={`http://localhost:8080/image/deal/${deal.image.storedFileName}`} alt="딜 이미지"/>
     <div className="flex flex-col justify-center">
-      <small className="font-normal text-sm text-gray-500">Station</small>
-      <h3 className="font-semibold text-lg">title</h3>
-      <div className="text-gray-700 text-sm">product</div>
+      <small className="font-normal text-sm text-gray-500">{deal.station}</small>
+      <h3 className="font-semibold text-lg">{deal.name}</h3>
+      <div className="text-gray-700 text-sm">{deal.product}</div>
       <div className="flex gap-x-2 items-center">
-        <strong className="text-blue-800 font-bold text-lg">00%</strong>
-        <div className="text-sm">00,000</div>
+        <strong className="text-blue-800 font-bold text-lg">{deal.discountRate}%</strong>
+        <div className="text-sm">{deal.price}</div>
       </div>
     </div>
   </div>;
