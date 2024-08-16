374e32dec0efebd5617068a9a65d2b29bcf3a7c8
cheoljundev <cheoljundev@gmail.com>
2024-08-16 14:45:39 +0900

    feat: 로그인 api 작업

--- src/components/user/LoginModal.jsx
+++ src/components/user/LoginModal.jsx
@@ -1,9 +1,14 @@
-import {forwardRef, useImperativeHandle, useRef} from 'react';
+import {forwardRef, useImperativeHandle, useRef, useState} from 'react';
 import {createPortal} from "react-dom";
 import {useNavigate} from "react-router-dom";
+import ky from "ky";
+import LoginAlert from "../user-course/LoginAlert.jsx";
 const LoginModal = forwardRef(function LoginModal({}, ref) {
   const modal = useRef();
   const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
   function handleSignup() {
     modal.current.close();
@@ -11,8 +16,33 @@ const LoginModal = forwardRef(function LoginModal({}, ref) {
   }
 
   function handleSignin() {
-    modal.current.close();
-    navigate("/");
+    ky.post("http://localhost:8080/api/signin", {
+      json: {username: id, password}
+    })
+      .text()
+      .then((data) => {
+        localStorage.setItem("token", data);
+        navigate("/");
+        modal.current.close();
+      })
+      .catch((error) => {
+        console.error("Failed to signin", error);
+        setError(true);
+      });
+  }
+
+  function handleId(e) {
+    setId(e.target.value);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleClose() {
+    setId("");
+    setPassword("");
+    setError(false);
   }
 
   useImperativeHandle(ref, () => {
@@ -24,7 +54,7 @@ const LoginModal = forwardRef(function LoginModal({}, ref) {
   })
 
   return createPortal(
-    <dialog ref={modal} id="my_modal_3" className="modal">
+    <dialog ref={modal} id="my_modal_3" className="modal" onClose={handleClose}>
       <div className="modal-box">
         <form method="dialog">
           {/* if there is a button in form, it will close the modal */}
@@ -32,20 +62,7 @@ const LoginModal = forwardRef(function LoginModal({}, ref) {
         </form>
         <h3 className="font-bold text-lg">로그인</h3>
         <p className="py-4">아이디와 비밀번호를 입력해 로그인해주세요.</p>
-        {/*<div role="alert" className="alert alert-error my-3">*/}
-        {/*  <svg*/}
-        {/*    xmlns="http://www.w3.org/2000/svg"*/}
-        {/*    className="h-6 w-6 shrink-0 stroke-current"*/}
-        {/*    fill="none"*/}
-        {/*    viewBox="0 0 24 24">*/}
-        {/*    <path*/}
-        {/*      strokeLinecap="round"*/}
-        {/*      strokeLinejoin="round"*/}
-        {/*      strokeWidth="2"*/}
-        {/*      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>*/}
-        {/*  </svg>*/}
-        {/*  <span>아이디 및 비밀번호가 맞지 않습니다.</span>*/}
-        {/*</div>*/}
+        {error && <LoginAlert/>}
         <div className="mb-4">
           <label className="input border-solid input-bordered flex items-center gap-2 mb-2">
             <svg
@@ -56,7 +73,7 @@ const LoginModal = forwardRef(function LoginModal({}, ref) {
               <path
                 d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"/>
             </svg>
-            <input type="text" className="grow" placeholder="Id"/>
+            <input type="text" value={id} className="grow" placeholder="Id" onChange={(e)=>handleId(e)}/>
           </label>
           <label className="input border-solid input-bordered flex items-center gap-2">
             <svg
@@ -69,7 +86,7 @@ const LoginModal = forwardRef(function LoginModal({}, ref) {
                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                 clipRule="evenodd"/>
             </svg>
-            <input type="password" className="grow" placeholder="Password"/>
+            <input type="password" value={password} className="grow" placeholder="Password" onChange={(e) => handlePassword(e)}/>
           </label>
         </div>
         <div className="flex flex-col gap-y-3 items-center">
