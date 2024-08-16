81372d6ff70d39ab132755cb31b478d3e4a00b67
cheoljundev <cheoljundev@gmail.com>
2024-08-16 10:31:53 +0900

    feat: Main UserCourseList 가져오기

--- src/components/home/user-course/UserCourseSection.jsx
+++ src/components/home/user-course/UserCourseSection.jsx
@@ -1,16 +1,34 @@
 import UserCourseSectionItemSkeleton from "./UserCourseSectionItemSkeleton.jsx";
 import UserCourseSectionItem from "./UserCourseSectionItem.jsx";
+import {useEffect, useState} from "react";
+import ky from "ky";
+import {Link} from "react-router-dom";
 export default function UserCourseSection() {
+  const [courses, setCourses] = useState(null); // 유저 코스
+  const [loading, setLoading] = useState(true); // 로딩 상태
+
+  useEffect(() => {
+    // 데이터 가져오기
+    ky.get("http://localhost:8080/api/course?size=5")
+      .json()
+      .then((data) => {
+        setCourses(data.content); // 데이터 저장
+        setLoading(false); // 로딩 완료
+      })
+      .catch((error) => {
+        console.error("Failed to fetch user courses", error);
+        setLoading(false); // 실패 시에도 로딩 완료
+      });
+  }, []);
+
   return (
     <section>
       <header className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-semibold">유저 추천 코스</h2>
-        <a href="">더보기</a>
+        <Link to="/user-course/list">더보기</Link>
       </header>
-      {/*<UserCourseSectionItemSkeleton/>*/}
-      <UserCourseSectionItem/>
-      <UserCourseSectionItem/>
-      <UserCourseSectionItem/>
+      {loading && <UserCourseSectionItemSkeleton/>}
+      {!loading && courses && courses.map((course) => <UserCourseSectionItem key={course.id} course={course}/>)}
     </section>
   );
 }
--- src/components/home/user-course/UserCourseSectionItem.jsx
+++ src/components/home/user-course/UserCourseSectionItem.jsx
@@ -1,21 +1,21 @@
 export default function UserCourseSectionItem({course}) {
   return <div className="flex gap-x-4 mb-4">
-    <img className="rounded-xl flex-shrink-0 self-start" src="https://picsum.photos/100" alt="유저코스 이미지"/>
+    <img className="rounded-xl flex-shrink-0 self-start" src={course.image} alt="유저코스 이미지"/>
     <div className="flex flex-col justify-center w-80">
-      <small className="font-normal text-sm text-gray-500">Station</small>
-      <h3 className="font-semibold text-lg">title</h3>
-      <div className="text-gray-700 text-sm">product</div>
+      <small className="font-normal text-sm text-gray-500">{course.state}</small>
+      <h3 className="font-semibold text-lg">{course.name}</h3>
       <div className="flex items-center justify-between">
         <div className="flex flex-col">
-          <div className="mb-4">0시간 00분</div>
+          <div className="mb-4">{course.time}</div>
           <div className="flex flex-wrap gap-2">
-            <span className="badge badge-outline border-solid">#공예</span>
-            <span className="badge badge-outline border-solid">#식사</span>
-            <span className="badge badge-outline border-solid">#식사</span>
-            <span className="badge badge-outline border-solid">#식사</span>
+            {
+              course.tags.map((tag, index) => {
+                return <span key={index} className="badge badge-outline border-solid">#{tag}</span>;
+              })
+            }
           </div>
         </div>
-        <div className="text-sm text-nowrap">좋아요 10</div>
+        <div className="text-sm text-nowrap">좋아요 {course.likes}</div>
       </div>
     </div>
   </div>;
