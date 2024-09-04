1d8303008cdc20a347c165331f13882cd06c8d6e
cheoljundev <cheoljundev@gmail.com>
2024-09-04 18:12:57 +0900

    feat: PlaceManage 완료

--- src/components/admin/course/PlaceManage.jsx
+++ src/components/admin/course/PlaceManage.jsx
@@ -1,102 +1,127 @@
 import PlaceItem from "../../course/PlaceItem.jsx";
+import PlaceItemSkeleton from "../../course/PlaceItemSkeleton.jsx";
+import {useEffect, useState} from "react";
+import ky from "ky";
+import Pagination from "../../util/Pagination.jsx";
+import {setMessage} from "../../redux/modules/modal.js";
+import {useDispatch} from "react-redux";
+import SearchPlaceHeader from "./place/SearchPlaceHeader.jsx";
 
 export default function PlaceManage() {
+  const [loading, setLoading] = useState(true);
+  const [places, setPlaces] = useState([]);
+  const [page , setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isFirst, setIsFirst] = useState(false);
+  const [isLast, setIsLast] = useState(false);
+  const dispatch = useDispatch();
+
+  function searchPlace(page = 0,
+                       placeType = '0000',
+                       largeCategory = '0000',
+                       middleCategory = '0000',
+                       tag = '0000',
+                       state = '0000',
+                       city = '0000')
+  {
+    setLoading(true);
+    setPlaces([]);
+
+    let url = `http://localhost:8080/api/manager/place?page=${page}&size=6`;
+
+    if (placeType !== '0000') {
+      url += `&placeTypeCode=${placeType}`;
+    }
+    if (largeCategory !== '0000') {
+      url += `&largeCategoryCode=${largeCategory}`;
+    }
+    if (middleCategory !== '0000') {
+      url += `&middleCategoryCode=${middleCategory}`;
+    }
+    if (tag !== '0000') {
+      url += `&tagCode=${tag}`;
+    }
+    if (state !== '0000') {
+      url += `&stateCode=${state}`;
+    }
+    if (city !== '0000') {
+      url += `&cityCode=${city}`;
+    }
+
+    ky.get(url,
+      {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }})
+      .json()
+      .then(data => {
+        setPage(page);
+        setPlaces(data.content);
+        setIsFirst(data.first);
+        setIsLast(data.last);
+        setTotalPages(data.totalPages);
+        setLoading(false);
+      })
+      .catch(() => {
+        dispatch(setMessage({
+          message: "장소 검색에 실패했습니다.",
+          isError: true,
+          isShow: true,
+        }))
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    if (page !== null){
+      searchPlace(page);
+    }
+  }, [page]);
+
+  function handleDeletePlace(place) {
+    // 컨펌창으로 확인
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      ky.delete(`http://localhost:8080/api/manager/place/${place.id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        }).json()
+        .then(() => {
+          searchPlace(page);
+        })
+        .catch(() => {
+          dispatch(setMessage({
+            message: "장소 삭제에 실패했습니다.",
+            isError: true,
+            isShow: true,
+          }))
+        });
+    }
+  }
+
   return (
     <div className="pb-12">
       <section className="px-4">
         <h2 className="mb-2 font-bold text-lg">장소 관리하기</h2>
-        <div className="card bg-base-100 w-full shadow-xl mb-6">
-          <div className="card-body">
-            <h3 className="card-title">검색 조건</h3>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">장소 타입</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" defaultValue="장소 타입 선택">
-                <option disabled>장소 타입 선택</option>
-                <option value="tag1">장소1</option>
-                <option value="tag2">장소2</option>
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">대분류</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" defaultValue="대분류 선택">
-                <option disabled>대분류 선택</option>
-                <option value="tag1">대분류1</option>
-                <option value="tag2">대분류2</option>
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">중분류</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" defaultValue="중분류 선택">
-                <option disabled>중분류 선택</option>
-                <option value="tag1">중분류1</option>
-                <option value="tag2">중분류2</option>
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">소분류</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" defaultValue="소분류 선택">
-                <option disabled>소분류 선택</option>
-                <option value="tag1">소분류1</option>
-                <option value="tag2">소분류2</option>
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">광역시/도</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" defaultValue="광역시/도 선택">
-                <option disabled>광역시/도 선택</option>
-                <option value="tag1">서울</option>
-                <option value="tag2">인천</option>
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">시/군/구</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" defaultValue="시/군/구 선택">
-                <option disabled>시/군/구 선택</option>
-                <option value="tag1">부천시</option>
-                <option value="tag2">소래포구</option>
-              </select>
-            </label>
-            <button className="btn btn-primary join-item flex-1">장소 검색하기</button>
-          </div>
-        </div>
+        <SearchPlaceHeader onSearch={searchPlace}/>
         <div className="card bg-base-100 w-full shadow-xl mb-6">
           <div className="card-body">
             <h3 className="card-title">검색 결과</h3>
             <label className="form-control w-full mb-4">
               <div className="card shadow-xl bg-gray-50 w-full">
                 <div className="card-body">
-                  {/*<PlaceItemSkeleton />*/}
-                  <PlaceItem type={"+"}/>
-                  <PlaceItem type={"+"}/>
-                  <PlaceItem type={"+"}/>
+                  {loading && <PlaceItemSkeleton/>}
+                  {
+                    places.map((place, index) => <PlaceItem key={index} place={place} onClick={handleDeletePlace}/>)
+                  }
                 </div>
               </div>
-              <div className="join m-4 mx-auto flex justify-center">
-                <button className="join-item btn btn-disabled">«</button>
-                <button className="join-item btn">1</button>
-                <button className="join-item btn btn-disabled">...</button>
-                <button className="join-item btn">100</button>
-                <button className="join-item btn">»</button>
-              </div>
+              <Pagination page={page} setPage={setPage} totalPages={totalPages} isFirst={isFirst} isLast={isLast}/>
             </label>
           </div>
         </div>
       </section>
-      <div className="join w-full fixed bottom-0">
-        <button className="btn btn-error join-item flex-1">삭제</button>
-      </div>
     </div>
   )
 }
