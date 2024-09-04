478c2c2e7c9cc5df10f5039e1ed034c59e45b30c
cheoljundev <cheoljundev@gmail.com>
2024-09-04 18:05:14 +0900

    ref: 장소 검색 헤더부 컴포넌트분리

--- src/components/admin/course/PlaceBring.jsx
+++ src/components/admin/course/PlaceBring.jsx
@@ -1,31 +1,12 @@
 import PlaceItem from "../../course/PlaceItem.jsx";
 import {Link, useNavigate} from "react-router-dom";
 import PlaceItemSkeleton from "../../course/PlaceItemSkeleton.jsx";
-import useField from "../../../hooks/useField.js";
-import useOption from "../../../hooks/useOption.js";
 import {useEffect, useState} from "react";
 import ky from "ky";
 import Pagination from "../../util/Pagination.jsx";
 import {useDispatch} from "react-redux";
 import {setMessage} from "../../redux/modules/modal.js";
-
-const initFieldsState = {
-  placeType: '0000',
-  largeCategory: '0000',
-  middleCategory: '0000',
-  tag: '0000',
-  state: '0000',
-  city: '0000',
-}
-
-const initOptionsState = {
-  placeTypeOptions: [],
-  largeCategoryOptions: [],
-  middleCategoryOptions: [],
-  tagOptions: [],
-  stateOptions: [],
-  cityOptions: [],
-}
+import SearchPlaceHeader from "./place/SearchPlaceHeader.jsx";
 
 export default function PlaceBring() {
 
@@ -39,55 +20,14 @@ export default function PlaceBring() {
   const [selectedPlaces, setSelectedPlaces] = useState([]);
   const dispatch = useDispatch();
 
-
-  const {fields, handleFieldChange, resetArrayFields} = useField(initFieldsState);
-
-  const {
-    placeType,
-    largeCategory,
-    middleCategory,
-    tag,
-    state,
-    city,
-  } = fields;
-
-  const {options, setOption} = useOption(initOptionsState);
-
-  const {
-    placeTypeOptions,
-    largeCategoryOptions,
-    middleCategoryOptions,
-    tagOptions,
-    stateOptions,
-    cityOptions,
-  } = options;
-
-  useEffect(() => {
-    setOption('placeTypeOptions');
-    setOption('stateOptions');
-  }, []);
-
-  useEffect(() => {
-    resetArrayFields(['largeCategory', 'middleCategory', 'tag']);
-    setOption('largeCategoryOptions', placeType);
-  }, [placeType]);
-
-  useEffect(() => {
-    resetArrayFields(['middleCategory', 'tag']);
-    setOption('middleCategoryOptions', largeCategory);
-  }, [largeCategory]);
-
-  useEffect(() => {
-    resetArrayFields(['tag']);
-    setOption('tagOptions', middleCategory);
-  }, [middleCategory]);
-
-  useEffect(() => {
-    resetArrayFields(['city']);
-    setOption('cityOptions', state);
-  }, [state]);
-
-  function searchPlace(page = 0) {
+  function searchPlace(page = 0,
+                       placeType = '0000',
+                       largeCategory = '0000',
+                       middleCategory = '0000',
+                       tag = '0000',
+                       state = '0000',
+                       city = '0000')
+  {
     setLoading(true);
     setPlaces([]);
 
@@ -179,90 +119,7 @@ export default function PlaceBring() {
     <div className="pb-12">
       <section className="px-4">
         <h2 className="mb-2 font-bold text-lg">장소 가져오기</h2>
-        <div className="card bg-base-100 w-full shadow-xl mb-6">
-          <div className="card-body">
-            <h3 className="card-title">검색 조건</h3>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">장소 타입</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" name="placeType" value={placeType} onChange={handleFieldChange}>
-                <option value="0000" disabled>장소 타입 선택</option>
-                {
-                  placeTypeOptions.map((option) => (
-                    <option key={option.code} value={option.code}>{option.name}</option>
-                  ))
-                }
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">대분류</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" name="largeCategory" value={largeCategory} onChange={handleFieldChange}>
-                <option value="0000" disabled>대분류 선택</option>
-                {
-                  largeCategoryOptions.map((option) => (
-                    <option key={option.code} value={option.code}>{option.name}</option>
-                  ))
-                }
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">중분류</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" name="middleCategory" value={middleCategory} onChange={handleFieldChange}>
-                <option value="0000" disabled>중분류 선택</option>
-                {
-                  middleCategoryOptions.map((option) => (
-                    <option key={option.code} value={option.code}>{option.name}</option>
-                  ))
-                }
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">소분류</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" name="tag" value={tag} onChange={handleFieldChange}>
-                <option value="0000" disabled>소분류 선택</option>
-                {
-                  tagOptions.map((option) => (
-                    <option key={option.code} value={option.code}>{option.name}</option>
-                  ))
-                }
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">광역시/도</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" name="state" value={state} onChange={handleFieldChange}>
-                <option value="0000" disabled>광역시/도 선택</option>
-                {
-                  stateOptions.map((option) => (
-                    <option key={option.code} value={option.code}>{option.name}</option>
-                  ))
-                }
-              </select>
-            </label>
-            <label className="form-control w-full max-w-xs mb-2">
-              <div className="label">
-                <span className="label-text font-semibold">시/군/구</span>
-              </div>
-              <select className="select select-bordered w-full max-w-xs mb-2" name="city" value={city} onChange={handleFieldChange}>
-                <option value="0000" disabled>시/군/구 선택</option>
-                {
-                  cityOptions.map((option) => (
-                    <option key={option.code} value={option.code}>{option.name}</option>
-                  ))
-                }
-              </select>
-            </label>
-            <button className="btn btn-primary join-item flex-1" onClick={()=>searchPlace(0)}>장소 검색하기</button>
-          </div>
-        </div>
+        <SearchPlaceHeader onSearch={searchPlace}/>
         <div className="card bg-base-100 w-full shadow-xl mb-6">
           <div className="card-body">
             <h3 className="card-title">검색 결과</h3>
