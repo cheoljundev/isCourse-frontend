4c3e08f0419d7a5e95e0f5d9c1bc75a15ac20eac
cheoljundev <cheoljundev@gmail.com>
2024-09-08 16:52:42 +0900

    fix: RecommendCourseDetail 완료

--- src/components/recommend-course/RecommendCourseDetail.jsx
+++ src/components/recommend-course/RecommendCourseDetail.jsx
@@ -1,11 +1,28 @@
 import CourseDetail from "../course/CourseDetail.jsx";
-import {useRef} from "react";
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import ky from "ky";
+import CourseDetailSkeleton from "../course/CourseDetailSkeleton.jsx";
 export default function RecommendCourseDetail() {
-  const modal = useRef();
+  const {id} = useParams();
+  const [course, setCourse] = useState(null);
+  const [loading, setLoading] = useState(true); // 로딩 상태
+  useEffect(() => {
+    ky.get(`http://localhost:8080/api/course/${id}`)
+      .json()
+      .then((data) => {
+        setCourse(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch course", error);
+        setLoading(false);
+      });
+  }, []);
   return (
     <section>
-      {/*<CourseDetailSkeleton/>*/}
-      <CourseDetail modal={modal}/>
+      {loading && <CourseDetailSkeleton/>}
+      {!loading && course && <CourseDetail course={course} id={id}/>}
     </section>
   )
 }
